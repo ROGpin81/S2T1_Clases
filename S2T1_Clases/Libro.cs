51e7afd922e85f454f9d469ca565ae3533ed6d34
ROGpin81 <roger.pinedab@gmail.com>
2025-08-08 00:43:41 -0600

    Create Libro.cs

---
+++ S2T1_Clases/Libro.cs
@@ -0,0 +1,26 @@
+﻿
+namespace S2T1_Clases
+{
+    public class Libro
+    {
+        //Declaración de propiedades/variables
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int AñoPublicacion { get; set; }
+
+        //Constructor
+        public Libro(string titulo, string autor, int añoPublicacion)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            AñoPublicacion = añoPublicacion;
+        }
+        // Método para mostrar información del libro
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Título: {Titulo}");
+            Console.WriteLine($"Autor: {Autor}");
+            Console.WriteLine($"Año de Publicación: {AñoPublicacion}");
+        }
+    }
+}
