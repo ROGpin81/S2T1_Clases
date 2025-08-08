748e730c7c6cbbf400e061bee71fe3c6d6167054
ROGpin81 <roger.pinedab@gmail.com>
2025-08-08 00:43:45 -0600

    Create Persona.cs

---
+++ S2T1_Clases/Persona.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace S2T1_Clases
+{
+    public class Persona
+    {
+        //Declaración de propiedades/variables
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int Edad { get; set; }
+
+        //Constructor
+        public Persona(string nombre, string apellido, int edad)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
+        }
+        // Método para mostrar información de la persona
+        public void Saludar()
+        {
+            Console.WriteLine($"Hola, mi nombre es {Nombre} {Apellido} y tengo {Edad} años.");
+        }
+    }
+}
